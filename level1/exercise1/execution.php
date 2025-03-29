e18a9ddd67ae99db59f8a2bbfbcc72e87c68721d
AlexPasquero <axelpasquero@gmail.com>
2025-03-29 18:11:32 +0100

    level1 exercise 1 peer corrected

--- level1/exercise1/execution.php
+++ level1/exercise1/execution.php
@@ -3,15 +3,25 @@ declare(strict_types=1);
 
 session_start();
 
-$name=$_POST['name'];
-$surname=$_POST['surname'];
-$age=$_POST['age'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['age'])) {
+        
+        $name = htmlspecialchars($_POST['name']);
+        $surname = htmlspecialchars($_POST['surname']);
+        $age = htmlspecialchars($_POST['age']);
+        
+        $_SESSION['name'] = $name;
+        $_SESSION['surname'] = $surname;
+        $_SESSION['age'] = $age;
 
+        echo "Name received: " . $name . "<br>";
+        echo "Surname received: " . $surname . "<br>";
+        echo "Age received: " . $age . "<br>";
 
-$_SESSION['name']=$name;
-$_SESSION['surname']=$surname;
-$_SESSION['age']=$age;
-
-echo "Name received: " . htmlspecialchars($name) . "<br>";
-echo "Surname received: " . htmlspecialchars($surname) . "<br>";
-echo "Age received: " . htmlspecialchars($age) . "<br>";
+        
+    } else {
+        echo "Error: All fields are required.";
+    }
+} else {
+    echo "Invalid request method.";
+}
